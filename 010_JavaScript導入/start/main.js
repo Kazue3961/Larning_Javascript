3c26f5e93dd766c2669401d6f2048472e2a1b709
Kazue.S <k.sakurai716@gmail.com>
2020-12-27 23:03:57 +0900

    [update]変数の宣言方法

--- 010_JavaScript導入/start/main.js
+++ 010_JavaScript導入/start/main.js
@@ -1,1 +1,16 @@
-alert('hello')
+// var, let ,const 
+let name = 'Tom'
+name = 'Tim'
+console.log('hello' + name)
+
+// Number, String, Undefined, Boolean, Null Symbol
+
+//動的型付け言語
+let variable = 'str'
+variable = 12;
+variable = undefined;
+variable = false;
+
+console.log(typeof variable);
+
+
