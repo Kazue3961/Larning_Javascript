8d5571aa4e5f18efb384958f7f1b5a1e58c25053
Kazue.S <k.sakurai716@gmail.com>
2020-12-27 23:55:07 +0900

    [first_commit]arrey

---
+++ 040_配列の使い方/start/main.js
@@ -0,0 +1,15 @@
+// const arry = [1,2,3,4,5,6, 'moji', false];
+// arry[3] = 8;
+// console.log(arry);
+
+// const arry = [1,2,3,4,5,6, 'moji', false];
+// arry.push('new item')
+// console.log(arry);
+
+// const arry = [1,2,3,4,5,6, 'moji', false];
+// arry.unshift('new item')
+// console.log(arry);
+
+const arry = [1,2,3,4,5,6, 'moji', false];
+arry.pop();
+console.log(arry);
