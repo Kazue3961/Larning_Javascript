eb2a996e636fe7539a1fe53e53d7ae6e5d2ddecb
Kazue.S <k.sakurai716@gmail.com>
2020-12-28 17:54:00 +0900

    [update]for_in/for_of

--- 060_ループの記述方法/start/main.js
+++ 060_ループの記述方法/start/main.js
@@ -4,6 +4,14 @@ const arry = [1,2,3,4,5,6];
 //     console.log(arry[i]);
 // }
 
-for(let i = 0; i < arry.length; i++) {
-    console.log([i]);
+// for(let i = 0; i < arry.length; i++) {
+//     console.log([i]);
+// }
+
+for(let i in arry) {
+    console.log(i, arry[i]);
+}
+
+for(let v of arry) {
+    console.log(v);
 }
