e3a8d8dc1ff12228288ee04448b2e240b4b7d515
Kazue.S <k.sakurai716@gmail.com>
2020-12-28 23:24:01 +0900

    [update]オブジェクトを含む配列の記述

--- 060_ループの記述方法/start/main.js
+++ 060_ループの記述方法/start/main.js
@@ -1,5 +1,7 @@
-const arry = [1,2,3,4,5,6];
+// const arry = [1,2,3,4,5,6];
 
+
+//.56 文法編　ループの記述方法基礎
 // for(let i = 0; i < arry.length; i = i + 1) {
 //     console.log(arry[i]);
 // }
@@ -8,10 +10,53 @@ const arry = [1,2,3,4,5,6];
 //     console.log([i]);
 // }
 
-for(let i in arry) {
-    console.log(i, arry[i]);
-}
 
-for(let v of arry) {
-    console.log(v);
+//.57 文法編　ループの記述方法（for inとfor of）
+// for(let i in arry) {
+//     console.log(i, arry[i]);
+// }
+
+// for(let v of arry) {
+//     console.log(v);
+// }
+
+
+//.58 文法編　ループの記述方法（実践）
+const todos = [
+    {
+        id: 1,
+        title: 'Go to school',
+        completed: true
+    },
+    {
+        id: 2,
+        title: 'Go to museum',
+        completed: true
+    },
+    {
+        id: 3,
+        title: 'Go shopping',
+        completed: false
+    }
+]
+
+for(let i = 0; i < todos.length; i++) {
+    let todo = todos[i];
+    if(todo.completed === true) {
+        console.log(i, todo.title);
+    }
+
+//for in
+for(let i in todos) {
+    let todo = todos[i];
+    if(todo.completed === true) {
+        console.log(i, todo.title);
+    }
+
+//for of
+for(let todo of todos) {
+    if(todo.completed === true) {
+        console.log(todo.title);
+    }
+    
 }
